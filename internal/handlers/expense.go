6eb7beb0b797843e953011d05da1c6d0bbf0011d
Ayush Kumat Anand <ayush.anand@octrotalk.com>
2025-12-04 15:36:43 +0530

    feat: add expense creation and user expense data retrieval endpoints, along with supporting models and handler logic.

--- internal/handlers/expense.go
+++ internal/handlers/expense.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"database/sql"
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/Ayush330/splitwise_server/internal/database"
@@ -33,7 +34,7 @@ func AddExpense(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 	RowsUpdated, err := result.RowsAffected()
-	if RowsUpdated == 0 {
+	if RowsUpdated == 0 || err != nil {
 		createExpenseRequestError("Cannot find group or user", http.StatusInternalServerError)
 		return
 	}
@@ -71,8 +72,13 @@ func GetUserGroupAndTheirExpenses(res http.ResponseWriter, req *http.Request) {
 	var body models.UserExpensesRequest
 	var tx *sql.Tx
 	var errResponse func(string, int)
+	var response []byte
 	errResponse = func(err string, statusCode int) {
 		http.Error(res, err, statusCode)
+	}
+	if err = json.NewDecoder(req.Body).Decode(&body); err != nil {
+		errResponse(err.Error(), http.StatusBadRequest)
+		return
 	}
 	tx, err = database.DB.Begin()
 	if err != nil {
@@ -81,5 +87,102 @@ func GetUserGroupAndTheirExpenses(res http.ResponseWriter, req *http.Request) {
 	}
 	defer tx.Rollback()
 	var result *sql.Rows
-	result, err = tx.Query("SELECT ")
+	queryString := `WITH usercte AS
+						(SELECT id FROM users WHERE uuid = UUID_TO_BIN(?)),
+						usergroupcte AS
+						(SELECT group_id FROM group_membership WHERE user_id IN (SELECT id FROM usercte)),
+						groupcte AS
+						(SELECT id, uuid, name FROM ugroups WHERE id IN (SELECT group_id FROM usergroupcte)),
+					 	transactioncte AS
+						(SELECT g.id AS group_id, g.uuid AS group_uuid, g.name AS group_name, tx.transaction_id AS transaction_id, tx.uuid AS transaction_uuid, tx.description AS transaction_description, tx.creator_id AS transaction_creator_id FROM transactions AS tx INNER JOIN groupcte AS g ON tx.rel_group = g.id),
+						finalcte AS
+						(SELECT u.uuid AS user_uuid, u.name, t.group_id AS group_id, t.group_uuid AS group_uuid, t.group_name AS group_name, t.transaction_id AS transaction_id, t.transaction_uuid AS transaction_uuid, t.transaction_description AS transaction_description, BIN_TO_UUID(t.transaction_creator_id) AS transaction_creator_id FROM users AS u INNER JOIN transactioncte AS t ON u.id = t.transaction_creator_id)	
+					SELECT BIN_TO_UUID(t.group_uuid) AS group_uuid, t.group_name,  BIN_TO_UUID(t.transaction_uuid) AS transaction_uuid, t.transaction_description, BIN_TO_UUID(t.user_uuid) AS user_uuid, t.name FROM expense as e INNER JOIN finalcte AS t ON e.transaction_id = t.transaction_id
+					`
+	result, err = tx.Query(queryString, body.UserId)
+	if err != nil {
+		errResponse(err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer result.Close()
+	type sqlRes struct {
+		GroupUuid              *string
+		GroupName              *string
+		TransactionUuid        *string
+		TransactionDescription *string
+		TransactionCreatorId   *string
+		UserName               *string
+	}
+	var resList = []sqlRes{}
+	var row sqlRes
+	for result.Next() {
+		result.Scan(&row.GroupUuid, &row.GroupName, &row.TransactionUuid, &row.TransactionDescription, &row.TransactionCreatorId)
+		resList = append(resList, row)
+	}
+	log.Println(resList)
+	err = tx.Commit()
+	if err != nil {
+		errResponse(err.Error(), http.StatusInternalServerError)
+		return
+	}
+	//convert to response data
+
+	type transactionStruct struct {
+		TransactionId          string `json:"transaction_id"`
+		TransactionDescription string `json:"transaction_description"`
+		TransactionCreatorId   string `json:"transaction_creator_id"`
+		CreatorName            string `json:"creator_name"`
+	}
+
+	type UserExpensesResponse struct {
+		GroupId      string              `json:"group_id"`
+		GroupName    string              `json:"group_name"`
+		Transactions []transactionStruct `json:"transactions"`
+	}
+
+	groupedResponses := make(map[string]UserExpensesResponse)
+
+	for _, r := range resList {
+		if r.GroupUuid == nil || r.GroupName == nil {
+			continue
+		}
+		groupUUID := *r.GroupUuid
+		groupName := *r.GroupName
+
+		if _, ok := groupedResponses[groupUUID]; !ok {
+			groupedResponses[groupUUID] = UserExpensesResponse{
+				GroupId:      groupUUID,
+				GroupName:    groupName,
+				Transactions: []transactionStruct{},
+			}
+		}
+
+		if r.TransactionUuid != nil {
+			currentGroup := groupedResponses[groupUUID]
+			currentGroup.Transactions = append(currentGroup.Transactions, transactionStruct{
+				TransactionId:          *r.TransactionUuid,
+				TransactionDescription: *r.TransactionDescription,
+				TransactionCreatorId:   *r.TransactionCreatorId,
+				CreatorName:            *r.UserName,
+			})
+			groupedResponses[groupUUID] = currentGroup
+		}
+	}
+
+	var finalResponse []UserExpensesResponse
+	for _, v := range groupedResponses {
+		finalResponse = append(finalResponse, v)
+	}
+
+	output := struct {
+		Response []UserExpensesResponse `json:"response"`
+	}{
+		Response: finalResponse,
+	}
+	response, err = json.Marshal(output)
+	if err != nil {
+		errResponse(err.Error(), http.StatusInternalServerError)
+		return
+	}
+	responseWrapper(http.StatusOK, response, res)
 }
